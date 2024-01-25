60878ae19d70bcaf4298df4b53fb526e4d01b592
Asan <704000320asan@gmail.com>
2024-01-26 00:58:35 +0600

    added Class names3

--- src/CountXX.java
+++ src/CountXX.java
@@ -1,6 +1,9 @@
-int countXX(String str) {
- int count = 0;
- for(int i = 0; i < str.length() - 1; i++){
-   if(str.substring(i, i + 2).equals("xx")) count ++;
- }return count;
+public class CountXX {
+    int countXX(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equals("xx")) count++;
+        }
+        return count;
+    }
 }
--- src/StringTimes.java
+++ src/StringTimes.java
@@ -1,6 +1,8 @@
+public class StringTimes {
+
 public String stringTimes(String str, int n) {
   String temp = "";
   for(int i = 0; i < n; i++)
   temp = temp + str;
   return temp;
-}
+}}
