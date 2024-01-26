6a3f8299a4fe14cd935c673e189c6deabd440e81
Asan <704000320asan@gmail.com>
2024-01-26 10:45:35 +0600

    added Splosion Calss

--- src/StringSplosion.java
+++ src/StringSplosion.java
@@ -1,6 +1,10 @@
-public String stringSplosion(String str) {
-  String res = "";
-  for(int i = 0; i < str.length() + 1; i ++){
-    res = res + str.substring(0,i);
-  }return res;
+public class StringSplosion {
+
+    public String stringSplosion(String str) {
+        String res = "";
+        for (int i = 0; i < str.length() + 1; i++) {
+            res = res + str.substring(0, i);
+        }
+        return res;
+    }
 }
